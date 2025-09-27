bc1f37a6fac59012e1b6cdbc9a9da3e54fa1fa0e
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-09-27 10:52:26 -0300

    updated api

--- index.js
+++ index.js
@@ -8,22 +8,42 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/cars", (req, res) => {
-  
+app.get("/cars", async (req, res) => {
+  try {
+    const allCars = await Car.findAll();
+
+    res.status(200).json(allCars);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+app.post("/cars", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newCar = await Car.create(req.body);
+
+    res.status(201).json(newCar);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Rotas de usuários
 // Rotas -> GET, POST, PUT, DELETE
 
-app.get("/users", async (req, res) => {
-  
-})
+app.get("/users", async (req, res) => {});
+
+// Rotas de usuários
+// Rotas -> GET, POST, PUT, DELETE
+
+app.get("/users", async (req, res) => {});
 
 sequelize
   .sync({ alter: true }) // Esse comando cria as tabelas no banco de dados, se elas não existirem
